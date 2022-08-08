04e1f968970247b4b45953ba5776998b146e17a4
jcrisostomo1 <joshuacr22@gmail.com>
2022-08-08 19:49:37 -0400

    add caesarCipher tests and code

---
+++ codes/caesarCipher.js
@@ -0,0 +1,31 @@
+const isLetter = (char) => {
+	return char.match(/[A-Za-z]/);
+}
+
+const isUpperCase = (char) => {
+	return char === char.toUpperCase();
+}
+
+const caesarCipher = (str, num) => {
+	const stringArray = [...str];
+	let newArray = [];
+
+	for (let char of stringArray) {
+		if (isLetter(char)) {
+			const charCode = char.toLowerCase().charCodeAt(0);
+			const shiftedCode = (((charCode - 97 + num) % 26) + 97);
+			const newLetter = String.fromCharCode(shiftedCode);
+
+			if (isUpperCase(char)) {
+				newArray.push(newLetter.toUpperCase());
+			} else {
+				newArray.push(newLetter);
+			}
+		} else {
+			newArray.push(char);
+		}
+	}
+	return newArray.join('');
+}
+
+export default caesarCipher;
